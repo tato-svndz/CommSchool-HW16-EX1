022210db3ee3dcbd91b967ebb988322b26066546
Tato Svndz <tato.svndz@gmail.com>
2023-08-16 16:48:27 +0400

    exercise 1

---
+++ script.js
@@ -0,0 +1,16 @@
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert(this.step);
+    }
+};
+
+ladder.up().down().up().up().up().showStep();
